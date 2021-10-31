8e1037013ce4f80d07c08c97804de16f7aa8ff2d
JohnBen <john@johnben.fr>
2021-10-31 13:03:22 +0100

    installation de conde silent mode corection color

--- wc.sh
+++ wc.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 # Version MAC et Linux
 #clear screen
@@ -7,15 +7,15 @@ reset
 echo "regular installation Mac"
 
 # Regular Colors
-CYAN='\033[0;36m'
-BLACK='\033[0;30m'
-BLUE='\033[34m'
-GREEN='\033[92m'
-MAGENTA='\033[95m'
-PURPLE='\033[0;35m'
-RED='\033[0;31m'
-WHITE='\033[0;37m'
-YELLOW='\033[0;33m'
+COLOR_CYAN='\033[0;36m'
+COLOR_BLACK='\033[0;30m'
+COLOR_BLUE='\033[34m'
+COLOR_GREEN='\033[92m'
+COLOR_MAGENTA='\033[95m'
+COLOR_PURPLE='\033[0;35m'
+COLOR_RED='\033[0;31m'
+COLOR_WHITE='\033[0;37m'
+COLOR_YELLOW='\033[0;33m'
 # Reset
 COLOR_OFF='\033[0m'
 
@@ -26,84 +26,84 @@ PYTHON_VERSION="-3.10.0-macos11"
 PYTHON_FILE_NAME="python"$PYTHON_VERSION".pkg"
 PYTHON_FILE_ABS_PATH=$WC_PATH/install/$PYTHON_FILE_NAME
 
+CONDA_NAME="Anaconda3"
 CONDA_VERSION="-2021.05-MacOSX-x86_64"
-CONDA_FILE_NAME="Anaconda3"$CONDA_VERSION".sh"
+CONDA_FILE_NAME=$CONDA_NAME$CONDA_VERSION".sh"
 CONDA_FILE_ABS_PATH=$WC_PATH/install/$CONDA_FILE_NAME
+
 
 #chmod +x test.sh
 
 
-echo "${MAGENTA}  __          __${BLUE}         _     _____ _        _   _"
-echo "${MAGENTA} \ \        / / ${BLUE}       | |   / ____| |      | | (_)"
-echo "${MAGENTA}  \ \  /\  / /${BLUE} __  _ __| | _| (___ | |_ __ _| |_ _  ___  _ __ "
-echo "${MAGENTA}   \ \/  \/ / ${BLUE} _ \| '__| |/ /\___ \| __/ _' | __| |/ _ \| '_ \  "
-echo "${MAGENTA}    \  /\  /${BLUE}  (_) | |  |   < ____) | || (_| | |_| | (_) | | | |"
-echo "${MAGENTA}     \/  \/${BLUE}  \___/|_|  |_|\_\_____/ \__\__,_|\__|_|\___/|_| |_|"
-echo "   ${MAGENTA}   _____${BLUE}             __ _                       _"
-echo "   ${MAGENTA} / ____|${BLUE}           / _(_)                     | |"
-echo "   ${MAGENTA}| |${BLUE}     ___  _ __ | |_ _  __ _ _   _ _ __ __ _| |_ ___  _ __"
-echo "   ${MAGENTA}| |${BLUE}    / _ \| '_ \|  _| |/ _' | | | | '__/ _' | __/ _ \| '__|"
-echo "   ${MAGENTA}| |___${BLUE}| (_) | | | | | | | (_| | |_| | | | (_| | || (_) | |"
-echo "   ${MAGENTA} \_____${BLUE}\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__\___/|_|"
+echo "${COLOR_MAGENTA}  __          __${COLOR_BLUE}         _     _____ _        _   _"
+echo "${COLOR_MAGENTA} \ \        / / ${COLOR_BLUE}       | |   / ____| |      | | (_)"
+echo "${COLOR_MAGENTA}  \ \  /\  / /${COLOR_BLUE} __  _ __| | _| (___ | |_ __ _| |_ _  ___  _ __ "
+echo "${COLOR_MAGENTA}   \ \/  \/ / ${COLOR_BLUE} _ \| '__| |/ /\___ \| __/ _' | __| |/ _ \| '_ \  "
+echo "${COLOR_MAGENTA}    \  /\  /${COLOR_BLUE}  (_) | |  |   < ____) | || (_| | |_| | (_) | | | |"
+echo "${COLOR_MAGENTA}     \/  \/${COLOR_BLUE}  \___/|_|  |_|\_\_____/ \__\__,_|\__|_|\___/|_| |_|"
+echo "   ${COLOR_MAGENTA}   _____${COLOR_BLUE}             __ _                       _"
+echo "   ${COLOR_MAGENTA} / ____|${COLOR_BLUE}           / _(_)                     | |"
+echo "   ${COLOR_MAGENTA}| |${COLOR_BLUE}     ___  _ __ | |_ _  __ _ _   _ _ __ __ _| |_ ___  _ __"
+echo "   ${COLOR_MAGENTA}| |${COLOR_BLUE}    / _ \| '_ \|  _| |/ _' | | | | '__/ _' | __/ _ \| '__|"
+echo "   ${COLOR_MAGENTA}| |___${COLOR_BLUE}| (_) | | | | | | | (_| | |_| | | | (_| | || (_) | |"
+echo "   ${COLOR_MAGENTA} \_____${COLOR_BLUE}\___/|_| |_|_| |_|\__, |\__,_|_|  \__,_|\__\___/|_|"
 echo "                             __/ |"
-echo "                            |___/            ${BLACK}Par JohnBen.ch${COLOR_OFF} \n"
+echo "                            |___/            ${COLOR_BLACK}Par JohnBen.ch${COLOR_OFF} \n"
 echo ""
-echo "${CYAN} ###########  ${YELLOW}Bonjour ${MAGENTA} ${CYAN} ########### ${COLOR_OFF}\n"
+echo "${COLOR_CYAN} ###########  ${COLOR_YELLOW}Bonjour ${COLOR_MAGENTA} ${COLOR_CYAN} ########### ${COLOR_OFF}\n"
 
 cd ~
 
 # Check download Folder
-if test -d "$WC_PATH" ; then
+if  [-d "$WC_PATH"] ; then
     cd $WC_PATH
 else
-    echo "${RED}Création du Répertoire${COLOR_OFF}"
+    echo "${COLOR_RED}Création du Répertoire${COLOR_OFF}"
     mkdir $WC_PATH"/install"
     cd $WC_PATH"/install"
-    echo "Dossier WC créé - ${GREEN}OK${COLOR_OFF} "
+    echo "Dossier WC créé - ${COLOR_GREEN}OK${COLOR_OFF} "
 fi
 
 # Check python install file
-echo "Control du fichier ${BLUE} ${STARTER_PY} ${COLOR_OFF}"
+echo "Control du fichier ${COLOR_BLUE} ${STARTER_PY} ${COLOR_OFF}"
 
 # Check Python
 if [[ "$(python3 -V 2> /dev/null))" =~ "Python 3" ]]  ; then
-    echo "${GREEN}Python est déjà installé :-)${COLOR_OFF}"
+    echo "${COLOR_GREEN}Python est déjà installé :-)${COLOR_OFF}"
 else
-    if test -f "$PYTHON_FILE_NAME" ; then
-        echo "${GREEN} $PYTHON_FILE_NAME ${RED}existe${COLOR_OFF}."
+    if [ -f "$PYTHON_FILE_NAME" ]; then
+        echo "${COLOR_YELLOW} $PYTHON_FILE_NAME ${COLOR_GREEN}existe${COLOR_OFF}."
     else
         echo "Récupération de Python"
-        curl -# -C - -o $PYTHON_FILE_ABS_PATH "https://www.python.org/ftp/python/3.10.0/${PYTHON_FILE_NAME}" && echo "Python récupéré - ${GREEN}OK${COLOR_OFF}"
+        curl -# -C - -o $PYTHON_FILE_ABS_PATH "https://www.python.org/ftp/python/3.10.0/${PYTHON_FILE_NAME}" && echo "Python récupéré - ${COLOR_GREEN}OK${COLOR_OFF}"
     fi
     sudo installer -verbose -pkg $PYTHON_FILE_ABS_PATH -target /
 fi
-
 # Check Anaconda
 if [[ "$(conda -V 2> /dev/null)" =~ "conda 4" ]]  ; then
-    echo "${GREEN}Anaconda est déjà installé :-)${COLOR_OFF}"
+    echo "${COLOR_GREEN}Anaconda est déjà installé :-)${COLOR_OFF}"
 else
-    if test -f "$CONDA_FILE_NAME" ; then
-        echo "${GREEN} $CONDA_FILE_NAME ${RED}existe${COLOR_OFF}."
+    if [ -f "$CONDA_FILE_NAME" ]; then
+        echo "${COLOR_YELLOW}$CONDA_FILE_NAME ${COLOR_GREEN}existe${COLOR_OFF}."
     else
         echo "Récupération de Anaconda"
-        curl -# -C - -o $CONDA_FILE_ABS_PATH "https://repo.anaconda.com/archive/${CONDA_FILE_NAME}" && echo "Anaconda récupéré - ${GREEN}OK${COLOR_OFF}"
+        # curl -# -C - -o $CONDA_FILE_ABS_PATH "https://repo.anaconda.com/archive/${CONDA_FILE_NAME}" && echo "Anaconda récupéré - ${COLOR_GREEN}OK${COLOR_OFF}"
+        curl -# -C - -o $CONDA_FILE_ABS_PATH "https://repo.anaconda.com/archive/${CONDA_FILE_NAME}" && echo "Anaconda récupéré - ${COLOR_GREEN}OK${COLOR_OFF}"
     fi
-    # sudo installer -verbose -pkg $CONDA_FILE_ABS_PATH -target ~
-    sh $CONDA_FILE_ABS_PATH
+
+    # Create Opt directory
+    if [ ! -d "$HOME/opt" ]; then
+        mkdir $HOME/opt
+    fi
+
+    FOLDER_INSTALL=$HOME/opt
+
+    #sh $CONDA_FILE_ABS_PATH -b -f -p ~/opt/$CONDA_NAME
+    export PATH="$HOME/opt/$CONDA_NAME/bin:$PATH"
+
+    # sudo installer -pkg $CONDA_FILE_ABS_PATH -target "${FOLDER_INSTALL}"
+    # sudo installer -pkg $CONDA_FILE_ABS_PATH -target $FOLDER_INSTALL
+
 fi
 
-
-
-
-#tar xvzf Python-2.7.3.tgz
-
-#conda create --name wc flask
-#conda activate wc
-#Echo Conda OK
-
-#!/usr/bin/env python
-
-#chmod +x test.py
-
-#python3 test.py
-
+read -p "Installation de Base Terminé !"
